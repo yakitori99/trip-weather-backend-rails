e7568164e4bccedbe59ac3f7786a75d7bc9f1706
yakitori99 <t2.akg3@gmail.com>
2021-11-23 01:27:26 +0900

    UpperCamelCaseでjsonから値を取得できるようテストを変更

--- test/controllers/prefs_controller_test.rb
+++ test/controllers/prefs_controller_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class PrefsControllerTest < ActionDispatch::IntegrationTest
 
-  test "should get index" do
+  test "should get_pref_all" do
     get prefs_url, as: :json
     # API戻り値をパース
     res = JSON.parse(@response.body)
@@ -11,9 +11,11 @@ class PrefsControllerTest < ActionDispatch::IntegrationTest
 
     # ソート順が正しいことを確認
     # assert_equal(expected, actual, [msg])
-    assert_equal(res[0]["pref_code"] , "01")
-    assert_equal(res[12]["pref_code"] , "13")
-    assert_equal(res[46]["pref_code"], "47")
+    assert_equal(res[0]["PrefCode"] , "01")
+    assert_equal(res[12]["PrefCode"] , "13")
+    assert_equal(res[46]["PrefCode"], "47")
+    # 値を確認
+    assert_equal(res[46]["PrefName"], "沖縄県")
 
     assert_response :success
   end
