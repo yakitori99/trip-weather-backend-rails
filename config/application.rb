2ccdc48ae084570a1dc90444a010f86326e6a09e
yakitori99 <t2.akg3@gmail.com>
2021-11-23 16:25:34 +0900

    タイムゾーンの指定を追加

--- config/application.rb
+++ config/application.rb
@@ -36,5 +36,8 @@ module TripWeatherBackendRails
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    # タイムゾーンを指定
+    config.time_zone = 'Asia/Tokyo'
   end
 end
